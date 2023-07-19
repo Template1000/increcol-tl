659685eb73792b29c78301a8305c6049bbcdb01a
Template1000 <template1000@proton.me>
2023-07-18 21:00:31 -0500

    fix 1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 import Script from 'next/script'
-import ScriptLoader from './components/ScriptLoader'
 import Main from './containers/Main/Main'
 
 export default function Home () {
@@ -7,10 +6,10 @@ export default function Home () {
     <>
       <Main />
       <Script src='/js/plugins/js/plugins.min.js' />
-      <Script src='/js/owl.carousel.js' />
-      <Script src='/js/theme.js' />
-      <ScriptLoader src='/js/theme.js' />
-      <ScriptLoader src='/js/theme.init.js' />
+      {/* <Script src='/js/owl.carousel.js' /> */}
+      {/* <Script src='/js/theme.js' /> */}
+      {/* <ScriptLoader src='/js/theme.js' /> */}
+      {/* <ScriptLoader src='/js/theme.init.js' /> */}
     </>
   )
 }

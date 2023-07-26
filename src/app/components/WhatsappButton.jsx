a0a65d4185080c976ad8bff8a73a6eb1d1f74af2
Template1000 <template1000@proton.me>
2023-07-26 13:02:39 -0500

    [FIX]

--- src/app/components/WhatsappButton.jsx
+++ src/app/components/WhatsappButton.jsx
@@ -5,7 +5,7 @@ import { FloatingWhatsApp } from 'react-floating-whatsapp'
 export default function WhatsappButton () {
   return (
     <FloatingWhatsApp
-      phoneNumber='+573229338849'
+      phoneNumber={`+57${process.env.NEXT_PUBLIC_PHONE_WHATSAPP}`}
       accountName='Javier Rodríguez'
       chatMessage={'¡Hola! 🤝 \n¿En qué podemos ayudarte?'}
       placeholder='Mensaje'

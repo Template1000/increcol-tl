8afde72d079d6d41c4f9e2d5af1854dc726102aa
Template1000 <135153131+Template1000@users.noreply.github.com>
2023-11-30 20:09:29 -0500

    FIX

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,16 @@
 import Script from 'next/script'
 import Main from './containers/Main/Main'
-// import ScriptLoader from './components/ScriptLoader'
+import ScriptLoader from './components/ScriptLoader'
 
 export default function Home () {
   return (
     <>
       <Main />
       <Script src='/js/plugins/js/plugins.min.js' />
-      <Script src='/js/owl.carousel.js' />
+      {/* <Script src='/js/owl.carousel.js' /> */}
       <Script src='/js/theme.js' />
-      {/* <ScriptLoader src='/js/theme.js' /> */}
-      {/* <ScriptLoader src='/js/theme.init.js' /> */}
+      <ScriptLoader src='/js/theme.js' />
+      <ScriptLoader src='/js/theme.init.js' />
     </>
   )
 }
